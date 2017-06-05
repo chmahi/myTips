2a9298ce9a70669b33407bb50b1d2eac5ebcb92c
chmahi <maheswari.chnaidu@gmail.com>
2017-06-05 18:41:57 +0530

    added css fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,9 @@ import {Nav, Platform, LoadingController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { FirstPage  } from '../pages/first-page/first-page';
-import { AdMob } from '@ionic-native/admob'
+import { AdMob } from '@ionic-native/admob';
+import { TipsService } from '../providers/tips-service';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -19,9 +21,7 @@ export class MyApp {
   rootPage:any =  FirstPage;
    pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen
- , public AdMob: AdMob
-  ) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public AdMob: AdMob, public tipsService: TipsService) {
     this.initializeApp();
     this.gform = new FormGroup({
     "gender": new FormControl({value: 'male', disabled: false})
@@ -32,9 +32,9 @@ export class MyApp {
         adId: 'ca-app-pub-7071565575097936/4327122006',
         isTesting: false
       };
-      AdMob.createBanner(options).then(() => {
-        AdMob.showBanner(8);
-      });
+      // AdMob.createBanner(options).then(() => {
+      //   AdMob.showBanner(8);
+      // });
   })
 
 
@@ -50,5 +50,8 @@ export class MyApp {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+  saveSettings(){
+   this.tipsService.getGender(this.gender);
   }
 }
